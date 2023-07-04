231072be26d39bd351a70baf9a2ab58d46f303d6
Julio Mateos Langerak <julio.mateos-langerak@igh.cnrs.fr>
2023-07-04 14:40:12 +0200

    Added test_load.py

---
+++ tests/test_load.py
@@ -0,0 +1,9 @@
+from numpy import ndarray
+import microscopemetrics_omero.load as load
+
+def test_load_image(conn, image_fixture, project_structure):
+    image_info = project_structure[2]
+    im_id = image_info[0][1]
+    image = conn.getObject("Image", im_id)
+    data = load.load_image(image)
+    assert type(data) == ndarray
