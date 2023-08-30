41a9c6fdb67c3b64c96192183250a02fe89d4e63
Julio Mateos Langerak <julio.mateos-langerak@igh.cnrs.fr>
2023-08-30 02:14:53 +0200

    local script update

--- src/local_scripts/run_metrics.py
+++ src/local_scripts/run_metrics.py
@@ -1,15 +1,43 @@
+import logging
+from io import StringIO
+from omero.gateway import BlitzGateway
+import yaml
+
+logger = logging.getLogger(__name__)
+log_string = StringIO()
+string_hdl = logging.StreamHandler(log_string)
+string_hdl.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    "%(asctime)s: %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+)
+string_hdl.setFormatter(formatter)
+logger.addHandler(string_hdl)
+
+
+
+def _read_config_from_file_ann(file_annotation):
+    return yaml.load(
+        file_annotation.getFileInChunks().__next__().decode(), Loader=yaml.SafeLoader
+    )
+
 
 def run_script_local():
-    from credentials import GROUP, HOST, PASSWORD, PORT, USER
 
-    conn = gateway.BlitzGateway(
-        username=USER, passwd=PASSWORD, group=GROUP, port=PORT, host=HOST
+    try:
+        with open("../microscopemetrics_omero/config/main_config.yaml", "r") as f:
+            main_config = yaml.load(f, Loader=yaml.SafeLoader)
+    except FileNotFoundError:
+        logger.error("No main configuration file found: Contact your administrator.")
+        return
+
+
+    conn = BlitzGateway(
+        username="root", passwd="omero", group="system", port=6064, host="localhost", secure=True
     )
 
     script_params = {
-        # "IDs": [154],
         "IDs": [1],
-        "Configuration file name": "yearly_config.ini",
+        "Configuration file name": "yearly_config.yaml",
         "Comment": "This is a test comment",
     }
 
@@ -22,6 +50,7 @@ def run_script_local():
         logger.info(f"Connection successful: {conn.isConnected()}")
 
         # Getting the configuration files
+        assay_config =
         analysis_config = MetricsConfig()
         analysis_config.read("main_config.ini")  # TODO: read main config from somewhere
         analysis_config.read(script_params["Configuration file name"])
@@ -31,21 +60,45 @@ def run_script_local():
 
         datasets = conn.getObjects(
             "Dataset", script_params["IDs"]
-        )  # generator of datasets
+        )
 
         for dataset in datasets:
-            process.process_dataset(
-                conn=conn,
-                script_params=script_params,
-                dataset=dataset,
-                analyses_config=analysis_config,
-                device_config=device_config,
-            )
+            microscope_prj = dataset.getParent()  # We assume one project per dataset
 
-        print(log_string.getvalue())
+            if microscope_prj is None:
+                logger.error(
+                    f"No parent project found for dataset {dataset.getName()}: "
+                    f"Every dataset must be part of a project and only one project."
+                )
+                continue
+
+            assay_conf_file_name = f"{script_params['Assay type']}_config.yaml"
+            assay_config = None
+            for ann in microscope_prj.listAnnotations():
+                if (
+                    type(ann) == gateway.FileAnnotationWrapper
+                    and ann.getFileName() == assay_conf_file_name
+                ):
+                    assay_config = _read_config_from_file_ann(ann)
+            if not assay_config:
+                logger.error(
+                    f"No assay configuration {assay_conf_file_name} found for dataset {dataset.getName()}: "
+                    f"Please contact your administrator"
+                )
+                continue
+
+            config = {
+                "script_parameters": script_params,
+                "main_config": main_config,
+                "assay_config": assay_config,
+            }
+            process.process_dataset(dataset=dataset, config=config)
+
+        logger.info(f"Metrics analysis finished")
 
     finally:
         logger.info("Closing connection")
+        print(log_string.getvalue())
         log_string.close()
         conn.close()
 
